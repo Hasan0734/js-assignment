007ea5612176395da4b565ab632d9ef1b563ae91
Hasan07 <Jahidhasan2856@gmail.com>
2021-05-29 23:13:02 +0600

    kilometer to meter and budget calculator function done

---
+++ assignment.js
@@ -0,0 +1,30 @@
+//Kilometer To Meter 
+// 1 kilometer  = 100 mter
+
+function kilometerToMeter(kilometer) {
+    var totalMeter = kilometer * 1000;
+    return totalMeter;
+}
+kilometerToMeter(15);
+
+
+// Budget Calculator
+// 1 watch = 50 tk
+// 1 phone = 100 tk
+// 1 laptop = 500 tk
+
+function budgetCalculator(watch, phone, laptop){
+    var watchPrice = watch * 50;
+    var phonePrice = phone  * 100;
+    var laptopPrice = laptop * 500;
+    var totalPrice = watchPrice + phonePrice + laptopPrice;
+    return totalPrice;
+}
+budgetCalculator(1, 1, 1);
+
+// Hotel Cost 
+// first 10 days = 100 tk night
+// next 10 days = 80 tk night
+function hotelCost () {
+
+}
